65a98f810225cf8c38b790631a02086b519246cc
andre <andre.jacques.pujol@gmail.com>
2023-03-07 09:24:34 -0600

    Added Cors to server  middleware

--- index.js
+++ index.js
@@ -1,28 +1,29 @@
 // inside index.js
-require('dotenv').config();
+require("dotenv").config();
 const PORT = 3000;
-const express = require('express');
+const express = require("express");
 const server = express();
-const morgan = require('morgan');
-server.use(morgan('dev'));
+const morgan = require("morgan");
+server.use(morgan("dev"));
+const cors = require("cors");
+server.use(cors());
 
-server.use(express.json())
-
+server.use(express.json());
 
 server.use((req, res, next) => {
-    console.log("<____Body Logger START____>");
-    console.log(req.body);
-    console.log("<_____Body Logger END_____>");
-  
-    next();
-  });
+  console.log("<____Body Logger START____>");
+  console.log(req.body);
+  console.log("<_____Body Logger END_____>");
 
-  const apiRouter = require('./api');
-server.use('/api', apiRouter);
+  next();
+});
 
-const { client } = require('./db');
+const apiRouter = require("./api");
+server.use("/api", apiRouter);
+
+const { client } = require("./db");
 client.connect();
 
 server.listen(PORT, () => {
-  console.log('The server is up on port', PORT)
+  console.log("The server is up on port", PORT);
 });
